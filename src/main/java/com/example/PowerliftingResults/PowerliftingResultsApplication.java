049f20d2ed3e8ec8449247c5d11aa633f69210d2
Areta Santos <aretasantoss@gmail.com>
2021-04-20 16:20:18 +0300

    change in application properties

--- src/main/java/com/example/PowerliftingResults/PowerliftingResultsApplication.java
+++ src/main/java/com/example/PowerliftingResults/PowerliftingResultsApplication.java
@@ -26,6 +26,8 @@ public class PowerliftingResultsApplication {
 	public CommandLineRunner Results(ResultRepository repository, UserRepository urepository) {
 		return (args) -> {
 			
+			
+			urepository.deleteAll();
 			User user1 = new User("user","$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "email1");
 			User user2 = new User("admin","$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN", "email2");
 					urepository.save(user1);
