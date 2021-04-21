8cbd87f707fef0b330886c1948c39ca458add7c4
Areta Santos <aretasantoss@gmail.com>
2021-04-21 16:00:51 +0300

    test with users

--- src/main/java/com/example/PowerliftingResults/PowerliftingResultsApplication.java
+++ src/main/java/com/example/PowerliftingResults/PowerliftingResultsApplication.java
@@ -29,10 +29,10 @@ public class PowerliftingResultsApplication {
 			
 			
 			//urepository.deleteAll();
-			User user1 = new User("user","$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "email1");
-			User user2 = new User("admin","$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN", "email2");
-					urepository.save(user1);
-					urepository.save(user2);
+			//User user1 = new User("user","$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "email1");
+			//User user2 = new User("admin","$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN", "email2");
+				//	urepository.save(user1);
+					//urepository.save(user2);
 			
 			//log.info("Save a few lifts");
 			
