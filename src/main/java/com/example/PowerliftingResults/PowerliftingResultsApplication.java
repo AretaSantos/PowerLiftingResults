a6943c11e625f6d4521f56fc7c5fb836c813ca25
Areta Santos <aretasantoss@gmail.com>
2021-04-21 16:39:41 +0300

    user test

--- src/main/java/com/example/PowerliftingResults/PowerliftingResultsApplication.java
+++ src/main/java/com/example/PowerliftingResults/PowerliftingResultsApplication.java
@@ -28,7 +28,7 @@ public class PowerliftingResultsApplication {
 		
 			
 			
-			/*urepository.deleteAll();
+			urepository.deleteAll();
 			
 			User user1 = new User("user","$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "email1");
 			User user2 = new User("admin","$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN", "email2");
@@ -37,13 +37,13 @@ public class PowerliftingResultsApplication {
 		
 			log.info("Save a few lifts");
 			
-			repository.save(new Result("penkkipunnerrus", 72 , "RPE9", "1", LocalDate.of(2021, 03, 21), user2));	
+			repository.save(new Result("penkkipunnerrus", 72 , "RPE9", "1", LocalDate.of(2021, 03, 21), user1));	
 			repository.save(new Result("kyykky", 130, "RPE10", "3", LocalDate.of(2021, 12, 10), user2));
 			
 			log.info("fetch all lifts");
 			for (Result result : repository.findAll()) {
 				log.info(result.toString());
-			}*/
+			}
 
 		};
 
